dde1661e1f09ebb66faa431f65b0fe58eaaf29c5
Matei Mircea <mrc_matei@yahoo.com>
2020-07-19 23:16:29 +0300

    Updates

---
+++ precache-manifest.f9726ea6ea2c70dd4061e54505a4e8f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4b6038c954e04f22e77defed3d0eb7",
+    "url": "/pixabay-api/index.html"
+  },
+  {
+    "revision": "20625a673f08f6e74f17",
+    "url": "/pixabay-api/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "224aba42eb0010424fdb",
+    "url": "/pixabay-api/static/css/main.2dc0fd43.chunk.css"
+  },
+  {
+    "revision": "20625a673f08f6e74f17",
+    "url": "/pixabay-api/static/js/2.61e534d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pixabay-api/static/js/2.61e534d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224aba42eb0010424fdb",
+    "url": "/pixabay-api/static/js/main.4c22590f.chunk.js"
+  },
+  {
+    "revision": "c6a47d09a6ba3382853f",
+    "url": "/pixabay-api/static/js/runtime-main.e4e9007e.js"
+  },
+  {
+    "revision": "033f49ac8cf391c4b036810da394f6c3",
+    "url": "/pixabay-api/static/media/2111575.033f49ac.svg"
+  }
+]);
