0e9ec133ec57e1944551af3848145ab18edf1ab6
Matei Mircea <mrc_matei@yahoo.com>
2020-07-25 21:07:11 +0300

    treia reparatie

--- src/App.js
+++ src/App.js
@@ -15,19 +15,17 @@ let API_KEY = '17538426-28a8ebd9211ecf8d22b6cde28';
 class App extends Component {
     state = {
       images: [],
-        error: null
+        error: "Please provide a value !!!"
     };
 
     howdy = async (event) => {
         event.preventDefault();
-        // console.log(event.target[0].value);
-        // console.log(event.target.searchValue.value);
         let serciValue = event.target.searchValue.value;
         let URL = `https://pixabay.com/api/?key=${API_KEY}&q=${serciValue}&image_type=photo&pretty=true`;
         let request = await fetch(URL);
         let response = await request.json();
         if (!serciValue) {
-            this.setState({ error: "Please provide a value" });
+            this.setState({ error: "Please provide a value !!!" });
         } else {
             this.setState({ images: response.hits, error: null });
         }
@@ -45,11 +43,13 @@ class App extends Component {
                         {this.state.error !== null ? <Route exact path="/" >
                                 <SearchButton appRequests={this.howdy} />
                                 <h1 style={{textAlign: 'center', marginBottom: '3rem'}}>{this.state.error}</h1>
-                            </Route> : <Route exact path="/" >
-                                <SearchButton appRequests={this.howdy} />
-                                <SearchButtonResults imagez={this.state.images} />
-                            </Route>
-                        }
+                            </Route> : this.state.images.length === 0 ? <Route exact path="/" >
+                            <SearchButton appRequests={this.howdy} />
+                            <h1 style={{textAlign: 'center', marginBottom: '3rem'}}>No result !!!</h1>
+                        </Route>: <Route exact path="/" >
+                            <SearchButton appRequests={this.howdy} />
+                            <SearchButtonResults imagez={this.state.images} />
+                        </Route> }
 
                         <Route exact path="/image/:id" component={Details} />
                         <Footer />
